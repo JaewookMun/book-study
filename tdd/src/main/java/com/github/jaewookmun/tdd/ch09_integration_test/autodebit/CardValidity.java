59649011a1db3f7dc0a6c3084ee3fe41f427d587
JaewookMun <mun1103.dev@gmail.com>
2024-08-21 01:49:31 +0900

    1. chapter 09 통합테스트   1) WireMock를 이용한 REST 클라이언트 테스트

---
+++ tdd/src/main/java/com/github/jaewookmun/tdd/ch09_integration_test/autodebit/CardValidity.java
@@ -0,0 +1,5 @@
+package com.github.jaewookmun.tdd.ch09_integration_test.autodebit;
+
+public enum CardValidity {
+    INVALID, ERROR, EXPIRED, UNKNOWN, THEFT, TIMEOUT, VALID
+}
---
+++ tdd/src/test/java/com/github/jaewookmun/tdd/ch09_integration_test/autodebit/CardNumberValidatorTest.java
@@ -0,0 +1,50 @@
+package com.github.jaewookmun.tdd.ch09_integration_test.autodebit;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CardNumberValidatorTest {
+
+    private WireMockServer wireMockServer;
+
+    @BeforeEach
+    void setUp() {
+        wireMockServer = new WireMockServer(options().port(8090));
+        wireMockServer.start();
+    }
+
+    @AfterEach
+    void tearDown() {
+        wireMockServer.stop();
+    }
+
+    @Test
+    void valid() {
+        wireMockServer.stubFor(post(urlEqualTo("/card"))
+                .withRequestBody(equalTo("1234567890"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "test/plain")
+                        .withBody("ok")));
+
+        CardNumberValidator validator = new CardNumberValidator("http://localhost:8089");
+        CardValidity validity = validator.validate("1234567890");
+        assertEquals(CardValidity.VALID, validity);
+    }
+
+    @Test
+    void timeout() {
+        wireMockServer.stubFor(post(urlEqualTo("/card"))
+                .willReturn(aResponse()
+                        .withFixedDelay(5000)));
+
+        CardNumberValidator validator = new CardNumberValidator("http://localhost:8089");
+        CardValidity validity = validator.validate("1234567890");
+        assertEquals(CardValidity.TIMEOUT, validator);
+    }
+}
