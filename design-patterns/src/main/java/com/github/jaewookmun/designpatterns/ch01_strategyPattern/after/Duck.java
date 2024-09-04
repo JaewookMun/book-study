e5dd53940d5940774e561e7622d4aa55d1573708
JaewookMun <mun1103.dev@gmail.com>
2024-09-05 02:41:16 +0900

    1. design-patterns   1) 전략패턴 (strategy) - 설명추가

--- design-patterns/src/main/java/com/github/jaewookmun/designpatterns/ch01_strategyPattern/after/Duck.java
+++ design-patterns/src/main/java/com/github/jaewookmun/designpatterns/ch01_strategyPattern/after/Duck.java
@@ -3,6 +3,10 @@ package com.github.jaewookmun.designpatterns.ch01_strategyPattern.after;
 import com.github.jaewookmun.designpatterns.ch01_strategyPattern.after.behavior.FlyBehavior;
 import com.github.jaewookmun.designpatterns.ch01_strategyPattern.after.behavior.QuackBehavior;
 
+/**
+ * 애플리케이션에서 달라지는 부분을 찾아내고, 달라지지 않는 부분으로부터 분리시킨다. <br>
+ * -> 바뀌는 부분은 따로 뽑아서 캡슐화시킨다.
+ */
 public abstract class Duck {
 
     FlyBehavior flyBehavior;
